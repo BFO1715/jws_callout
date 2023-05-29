9d1addeb071ed13b4ddd86d46c8e7e12094d6183
BFO1715 <bethan@johnwhiteandson.com>
2023-05-29 12:23:26 +0000

    Added lists and search options for requests and comments

--- callout_request/admin.py
+++ callout_request/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import Request
+from .models import Request, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Request)
 class RequestAdmin(SummernoteModelAdmin):
 
+    list_display = ('description', 'slug', 'created_on')
+    search_fields = ['description', 'content']
+    list_filter = ('description', 'created_on')
+    prepopulated_fields = {'slug': ('description',)}
     summernote_fields = ('content',)
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'request', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
