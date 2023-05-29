07737766f635b961e41ab0052a1840d28f563ef9
BFO1715 <bethan@johnwhiteandson.com>
2023-05-29 11:12:05 +0000

    Made migrations

--- callout_request/models.py
+++ callout_request/models.py
@@ -21,7 +21,7 @@ class Request(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+    post = models.ForeignKey(Request, on_delete=models.CASCADE,
                              related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
