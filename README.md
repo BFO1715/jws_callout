1cc8bf29a7e95e6b5793c110f7e8ee0af3b0b5d0
BFO1715 <bethan@johnwhiteandson.com>
2023-05-29 17:39:10 +0000

    Styled add_request

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ APi Key
 python3 manage.py makemigrations<br>
 python3 manage.py migrate<br>
 python3 manage.py runserver
+
+import os
+
+os.environ["DATABASE_URL"]="postgres://altiihsq:Vecuor_Jvir81c122tOv3w-qRkmRBnXN@lucky.db.elephantsql.com/altiihsq
+os.environ["SECRET_KEY"]="my-jws-secret-key"
+os.environ["CLOUDINARY_URL"]="cloudinary://798566658129613:ExOirMglR8aUVwNJdFs86BDFk6U@diplwfbus "
--- callout_request/forms.py
+++ callout_request/forms.py
@@ -1,16 +1,18 @@
 from django import forms
-from.models import Request
+from .models import Request
 
 class RequestForm(forms.ModelForm):
-    class meta:
+    class Meta:
         model = Request
         fields = ('description', 'slug', 'featured_image', 'author', 'excerpt', 'content')
 
         widgets = {
             'description': forms.TextInput(attrs={'class': 'form-control'}),
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
-            'featured_image': forms.ImageField(attrs={'class': 'form-control'}),
+            'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'excerpt': forms.Textarea(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+        
