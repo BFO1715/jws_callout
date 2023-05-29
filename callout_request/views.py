d8adecd0d951b6766bdff15c943a2c1ae30a2488
BFO1715 <bethan@johnwhiteandson.com>
2023-05-29 20:13:38 +0000

    Installed crispy formms to enhance comment section

--- callout_request/views.py
+++ callout_request/views.py
@@ -26,10 +26,37 @@ class RequestDetail(View):
             {
                 "request": request,
                 "comments": comments,
+                "commented": False,
                 "comment_form": CommentForm()
             },
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Request.objects.filter(slug=slug)
+        request_obj = get_object_or_404(queryset, slug=slug)
+        comments = request_obj.comments.filter(approved=True).order_by("-created_on")
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.rewuest = request
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        
+
+        return render(
+            request,
+            "callout_request.html",
+            {
+                "request": request,
+                "comments": comments,
+                "commented": True,
+                "comment_form": CommentForm()
+            },
+        )
 
 class AddRequest(generic.CreateView):
     model = Request
