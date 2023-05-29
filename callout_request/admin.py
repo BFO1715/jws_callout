9a4343295ed6b914734f1beedc313a8f1217e195
BFO1715 <bethan@johnwhiteandson.com>
2023-05-29 12:13:11 +0000

    Added summernote for content box

--- callout_request/admin.py
+++ callout_request/admin.py
@@ -6,9 +6,4 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Request)
 class RequestAdmin(SummernoteModelAdmin):
 
-    list_display = ('description', 'slug', 'created_on')
-    search_fields = ['description', 'content']
-    list_filter = ('created_on')
-    prepopulated_fields = {'slug': ('description',)}
     summernote_fields = ('content',)
-
